c5738b7cdb81b86c56fb54316acfa49b4b948e27
Maria-Lucia-Mazzara <marialuciaboolen@gmail.com>
2025-10-22 14:06:48 +0200

    snck 3 fatto esercizio usando il metodo map

--- snack-3/script.js
+++ snack-3/script.js
@@ -3,7 +3,7 @@ const numbers = [2, 8, 4, 7, 2, 87];
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
 
-const increment = [];
+// const increment = [];
 
 // // ciclo for classico 
 // for (let i = 0; i < numbers.length; i++) {
@@ -12,7 +12,16 @@ const increment = [];
 // }
 // console.log(increment);
 
-numbers.forEach((number) => {
-increment.push(number + 1);
+// // forEach
+// numbers.forEach((number) => {
+// increment.push(number + 1);
+// });
+// console.log(increment);
+
+// metodo map perchè devo fare un'arraynuova e farmela restituire
+
+const increment = numbers.map( (numero) => {
+    return numero + 1;
 });
 console.log(increment);
+
