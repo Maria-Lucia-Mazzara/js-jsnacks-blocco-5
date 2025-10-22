e7ddcd32093259ac595b78263fa53becc51c5b8b
Maria-Lucia-Mazzara <marialuciaboolen@gmail.com>
2025-10-22 13:33:29 +0200

    snack 1 fatto sia ciclo for classico che il forEach

--- snack-1/script.js
+++ snack-1/script.js
@@ -3,8 +3,15 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
 
+// ciclo for classico
 
 for (let i = 0; i < names.length; i++) {
     const name = names[i];
     console.log(name);
 }
+
+// soluzione forEach
+
+names.forEach((name) => {
+  console.log(name);
+});
