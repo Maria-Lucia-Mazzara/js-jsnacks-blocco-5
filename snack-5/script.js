24ccfe4b636951769bad8c87c2bea3777ccbbd8e
Maria-Lucia-Mazzara <marialuciaboolen@gmail.com>
2025-10-22 15:03:15 +0200

    snack5 fatto metodo ciclo for

--- snack-5/script.js
+++ snack-5/script.js
@@ -2,3 +2,16 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+const numeri_pari = [];
+
+// metodo ciclo for 
+for (let i = 0; i < nums.length; i++) {
+    const number = nums[i];
+
+    if (number % 2 === 0) {
+        numeri_pari.push(number)
+    }
+    
+}
+console.log(numeri_pari);
