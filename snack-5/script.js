2788dc77e91fc2a67a168a3aaf6cd15c19c9aaa7
Maria-Lucia-Mazzara <marialuciaboolen@gmail.com>
2025-10-22 15:25:21 +0200

    dello sneck 5 fatto metodo forEach

--- snack-5/script.js
+++ snack-5/script.js
@@ -3,15 +3,26 @@ const nums = [2, 8, 4, 7, 12, 87];
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
 
+// const numeri_pari = [];
+
+// // metodo ciclo for 
+// for (let i = 0; i < nums.length; i++) {
+//     const number = nums[i];
+
+//     if (number % 2 === 0) {
+//         numeri_pari.push(number)
+//     }
+
+// }
+// console.log(numeri_pari);
+
 const numeri_pari = [];
+//  metodo forEach
 
-// metodo ciclo for 
-for (let i = 0; i < nums.length; i++) {
-    const number = nums[i];
-
+nums.forEach((number) => {
     if (number % 2 === 0) {
-        numeri_pari.push(number)
+        numeri_pari.push(number);
     }
-    
-}
-console.log(numeri_pari);
+});
+
+console.log(numeri_pari); 
